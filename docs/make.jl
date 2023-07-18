c6df9ce0fc94463bf2642e87c673cc062e6c0d3d
os-rss <roman.samarev@opensesame.com>
2023-07-18 16:36:15 -0700

    docs: minor structure optimization

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,18 @@ using Documenter
 using GptSearchPlugin
 
 makedocs(
-    sitename = "GptSearchPlugin",
-    format = Documenter.HTML(),
-    modules = [GptSearchPlugin],
-    pages = [
+    sitename="GptSearchPlugin",
+    format=Documenter.HTML(),
+    modules=[GptSearchPlugin],
+    pages=[
         "Home" => "index.md",
         "API" => "API/index.md",
+        "Vector storage providers" => [
+            "Getting Started" => "Providers/index.md",
+            "Elasticsearch" => "Providers/Elasticsearch/index.md",
+        ],
         "Internals" => "Internals/index.md"
     ]
 )
 
-deploydocs(repo = "github.com/OpenSesame/GptSearchPlugin.git")
+deploydocs(repo="github.com/OpenSesame/GptSearchPlugin.git")
